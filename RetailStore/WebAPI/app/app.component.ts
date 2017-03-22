ee0193e1fdba2d42e4f54017216b10172f09f83f
Evgeniy Yatskov <djclever21@gmail.com>
2017-03-22 10:03:18 +0300

    Moved html template into separate file.

--- RetailStore/WebAPI/app/app.component.ts
+++ RetailStore/WebAPI/app/app.component.ts
@@ -11,61 +11,7 @@ import { NgForm } from '@angular/forms';
 
 @Component({
     selector: 'my-app',
-    template: ` <div class='row'>
-                    <form>
-                     <select class="form-control" [(ngModel)]="addPurchase.accessoryId" name="addId" (ngModelChange)="onSelect()">
-                        <option *ngFor="let accessory of accessories" [ngValue]="accessory.accessoryId">{{accessory.accessoryName}}</option>
-                     </select>
-                     <div>
-                        <label for="clientName">ClientName *</label>
-                        <input type="text" class="form-control"
-                            [(ngModel)]="addPurchase.clientName" name="clientName" required>
-                    </div>
-                    <div>
-                        <label for="pQuantity">Quantity *</label>
-                        <input type="number" class="form-control"
-                            [(ngModel)]="addPurchase.quantity" name="addQuantity" required>
-                    </div>
-                    <div>
-                        <button type="button" (click)="onAdd(addPurchase)"
-                            class="btn btn-primary">Add
-                        </button>
-                    </div>
-                    </form>
-                </div>
-                <p></p>
-                <div class='row'>
-                  <div class="panel panel-default">                
-                    <div class='panel-heading'>Purchases List</div>
-                    <div class='panel-body'>
-                    <table class='table table-condensed'>
-                      <thead>
-                        <th>PurchaseId</th>
-                        <th>AccessoryId</th>
-                        <th>ClientId</th>
-                        <th>Quantity</th>
-                        <th>PurchaseDate</th>
-                      </thead>
-                      <tbody>
-                        <tr *ngFor="let purchase of purchases">
-                          <td>{{purchase?.purchaseId}}</td>
-                          <td>{{purchase?.accessoryId}}</td>
-                          <td>{{purchase?.clientId}}</td>
-                          <td><input type="text" 
-                            [(ngModel)]="purchase.quantity"  name="quantity" class="form-control" /> </td>
-                          <td>{{purchase?.purchaseDate}}</td>
-                          <td> <input type="button" 
-                            value="BuyExtra" class="btn btn-default" 
-                            (click)="onUpdate(purchase)"/> 
-                            <input type="button" value="Redo" 
-                            class="btn btn-danger"  
-                            (click)="onDelete(purchase.purchaseId)"/></td>
-                        </tr>
-                      </tbody>
-                    </table>
-                    </div>
-                  </div>
-                </div>`,
+    templateUrl: `app/app.component.html`,
     providers: [HttpService]
 })
 export class AppComponent implements OnInit {
